a7159bbae1d09d328b86a21ab997e7bcac0c3239
mizushima1226 <mizuki.bz.3224@gmail.com>
2020-03-02 21:20:22 +0900

    refactor events with reducer

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -34,7 +34,7 @@ const EventForm = () => {
 
     const unCreatable = title === '' || body === ''
 
-    const unDeletable = state.length === 0
+    const unDeletable = state.events.length === 0
 
     return(
         <>
